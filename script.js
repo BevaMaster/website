e0486b4c38c7195514b3535de0648d47159af0d8
BevaMaster <129535807+BevaMaster@users.noreply.github.com>
2025-05-20 00:39:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("registerForm")?.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const username = document.getElementById("newUsername").value;
+  const password = document.getElementById("newPassword").value;
+  localStorage.setItem(username, password);
+  alert("Registrasi berhasil!");
+  window.location.href = "login.html";
+});
+
+document.getElementById("loginForm")?.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const savedPassword = localStorage.getItem(username);
+  if (savedPassword === password) {
+    alert("Login berhasil!");
+    window.location.href = "../website2/contact.html";
+  } else {
+    alert("Username atau password salah.");
+  }
+});
