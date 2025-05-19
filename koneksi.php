3e4e4c5d89599186f1beca4551bab07454b27f19
BevaMaster <129535807+BevaMaster@users.noreply.github.com>
2025-05-20 00:29:18 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "upload-file";
-
-$koneksi = mysqli_connect($server, $username, $password, $dbname) or die("Koneksi ke database gagal");
